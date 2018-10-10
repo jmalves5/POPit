d3ed4d027ccfe100360184b6c4332c5e719980ee
joaoalves <jmalves5@hotmail.com>
2018-10-10 13:11:30 +0100

    fixed spawning and deletion of objects

---
+++ Assets/Ball.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private Vector3 Direction;
+    public float Velocity;
+    private bool counting;
+    private bool death;
+    private Vector3 oldPos;
+    private Vector3 aux;
+    public Ball BounceBall;
+
+   
+    void Start ()
+    {
+        Velocity = 200;
+        Direction = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0);
+        Direction.Normalize(); // equivalente a dizer direction = direcion.normalized;
+        counting = false;
+    }
+	
+	
+	void Update ()
+    {
+        if (!counting)
+            StartCoroutine(WaitTime(30f));
+
+        transform.position += Direction * Velocity * Time.deltaTime;
+	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bool wrongCollision = false;
+        Vector2 normal = collision.contacts[0].normal;
+        EdgesCollisions edgesCollisions = collision.transform.GetComponent<EdgesCollisions>();
+
+        if (!wrongCollision)
+        {
+            Direction = Vector2.Reflect(Direction, normal);
+            Direction.Normalize();
+        }
+
+    }
+
+    public IEnumerator WaitTime(float time2Count)
+    {
+        Debug.Log("it's ya boy");
+        counting = true;
+        yield return new WaitForSecondsRealtime(time2Count);
+        Debug.Log("it's ya boy2");
+        Destroy(transform.gameObject);
+        counting = false;
+    }
+}
---
+++ Assets/BallCreation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCreation : MonoBehaviour {
+    private bool spawn;
+    public Ball BounceBall;
+    private bool waiting;
+    private Vector3 aux;
+
+	// Use this for initialization
+	void Start () {
+        spawn = true;
+        waiting = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!waiting)
+            StartCoroutine(WaitTime(5f)); //Ball generation time
+
+        if (spawn) {
+            aux.x = Random.Range(-450, 450);
+            aux.y = Random.Range(-250, 250);
+            Ball newBall = Instantiate(BounceBall, aux, BounceBall.transform.rotation);
+            //BounceBall.transform.SetParent(GameObject.Find("GameCanvas").transform, false);
+            spawn = false;
+        }
+    }
+
+    public IEnumerator WaitTime(float time2Count)
+    {
+        waiting = true;
+        yield return new WaitForSecondsRealtime(time2Count);
+        waiting = false;
+        spawn = true;
+        
+    }
+}
